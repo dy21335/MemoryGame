a8961c5e237bf8b3b8694310d4c644ba10f2760e
dy21335 <daisy_neuq@163.com>
2017-12-13 21:59:38 +0800

    finish

--- js/success.js
+++ js/success.js
@@ -1,13 +1,15 @@
 window.onload=function(){
     var restartB,
         moves,
-        stars;
+        stars,
+        time;
     restartB=document.getElementById('success');
     moves=document.getElementById('moves');
     stars=document.getElementById('stars');
-    console.log(localStorage.getItem("moves1"));
+    time=document.getElementById('time');
     moves.innerHTML=localStorage.getItem("moves1");
     stars.innerHTML=localStorage.getItem("stars1");
+    time.innerHTML=localStorage.getItem("time1");
     if (restartB) {
         restartB.addEventListener('click', function (e) {
             window.open('index.html', '_self');
--- js/values.js
+++
@@ -1,3 +0,0 @@
-window.moves1=0;
-window.time1=0;
-window.stars1=0;
